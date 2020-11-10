7ff360d3fa02378e1a4001feaef71f139e13e570
Daniel Wiehoff <daniel.wiehoff+github@gmail.com>
2020-11-10 15:18:13 +0100

    Migrated to add author column to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_134558) do
+ActiveRecord::Schema.define(version: 2020_11_10_141615) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_11_10_134558) do
     t.float "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "author"
   end
 
 end
